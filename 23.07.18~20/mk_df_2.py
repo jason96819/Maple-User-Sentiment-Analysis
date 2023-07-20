51110b4e56975475cf107a39d9a6b50eb518b2a1
Jason Jung <74489776+jason96819@users.noreply.github.com>
2023-07-20 23:01:30 +0900

    Update mk_df_2.py

--- 23.07.18~20/mk_df_2.py
+++ 23.07.18~20/mk_df_2.py
@@ -6,8 +6,8 @@ import urllib.request
 from urllib.error import HTTPError
 import module as m
 
-df = m.mk_df(1800001, 1845250)                                                  #1768970 ~ 1845249    /   1591880(6/1), 2019732(7/13)
+df = m.mk_df(1591880, 1845250)                                                #1591880(6/1) ~ 1845249(6/30)
 
 
-filename = r'C:\Users\user\Documents\nexon\maple_inven_all_3.csv'               # 저장할 파일 경로와 이름 지정
-df.to_csv(filename, index=False)                                                # 데이터프레임을 CSV 파일로 저장
+filename = r'C:\Users\user\Documents\nexon\maple_inven_all.csv'               # 저장할 파일 경로와 이름 지정
+df.to_csv(filename, index=False)                                              # 데이터프레임을 CSV 파일로 저장
